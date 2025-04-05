8d3e954d974c1dd8560bdcf9b9d3efb09a991401
Ian Nappi <ian.nappi@gwmail.gwu.edu>
2025-04-04 22:27:28 -0400

    updated skills and added a project desc.

--- js/event_listeners.js
+++ js/event_listeners.js
@@ -7,3 +7,13 @@ document.getElementById('link').addEventListener('mouseout', function() {
     var img = document.getElementById('img');
     img.style.backgroundImage = "url('static/github.png')";
 });
+
+// mouseover for trash planet github link
+document.getElementById('link_trashp').addEventListener('mouseover', function() {
+    var img = document.getElementById('img_trashp');
+    img.style.backgroundImage = "url('static/50-transparent-github.png')";
+});
+document.getElementById('link_trashp').addEventListener('mouseout', function() {
+    var img = document.getElementById('img_trashp');
+    img.style.backgroundImage = "url('static/github.png')";
+});
